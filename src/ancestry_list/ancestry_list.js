96f954d806d652e6d75f9235cd33034b7c5e142c
God Emperor Joakim <kjronning@gmail.com>
2019-08-02 23:28:21 +0200

    Ancestry and heritage selectbox ready.

--- src/ancestry_list/ancestry_list.js
+++ src/ancestry_list/ancestry_list.js
@@ -2,30 +2,72 @@ import React,{Component} from 'react';
 import ListData from '../data/ancestry.json';
 
 
-class AncestryList extends Component {
-    render() {
-        const ancestryList = ListData.map(list => {
-            return <li>{list.ancestry}</li>
-        });
 
+
+class AncestryList extends Component {
+
+    defaultAncestry = "Select ancestry...";
+    defaultHeritage = "Select heritage...";
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            ancestry: this.defaultAncestry,
+            heritage: this.defaultHeritage
+        };
+        this.handleAncestryChange = this.handleAncestryChange.bind(this);
+        this.handleHeritageChange = this.handleHeritageChange.bind(this);
+    }
+
+    handleAncestryChange(event) {
+        this.setState({ancestry: event.target.value});
+    }
+
+    handleHeritageChange(event) {
+        this.setState({heritage: event.target.value});
+    }
+
+    renderAncestrySelect(){
         return (
             <div>
-                <ul>
-                    {ancestryList}
-                </ul>
-                {/*{ListData.map((list) => {*/}
-                {/*    return <div>*/}
-                {/*        <h1>{list.ancestry}</h1>*/}
-                {/*        {list.heritages.map((sublist) => {*/}
-                {/*            return <div>*/}
-                {/*                <p>{sublist.name}</p>*/}
-                {/*            </div>*/}
-                {/*        })}*/}
-                {/*    </div>*/}
-                {/*})}*/}
+                <p>{this.state.ancestry}</p>
+                <select value={this.state.ancestry} onChange={this.handleAncestryChange}>
+                    <option defaultValue>{this.defaultAncestry}</option>
+                    {ListData.map(list => {
+                            return <option value={list.ancestry}>{list.ancestry}</option>
+                        }
+                    )}
+                </select>
+                {(this.state.ancestry === this.defaultAncestry) ? null : this.renderHeritageSelect()}
+            </div>
+
+        )
+    }
+
+    renderHeritageSelect() {
+        if (this.state.ancestry === this.defaultAncestry)
+            return;
+        return (
+            <div>
+            <select value={this.state.heritage} onChange={this.handleHeritageChange}>
+                <option defaultValue>{this.defaultHeritage}</option>
+                {ListData.map(list => {
+                    return list.ancestry === this.state.ancestry ? list.heritages.map(sublist => {
+                        return <option value={sublist.name}>{sublist.name}</option>
+                    }) : null
+                })}
+            </select>
             </div>
         )
     }
+
+
+    render() {
+        return (
+        this.renderAncestrySelect()
+    )
+    }
 }
+
 
 export default AncestryList;
